fcb9a7e5f45338350affd84c4cfbbf9fc2235c84
genzj <zj0512@gmail.com>
2019-08-26 00:41:04 +0800

    add xfce setter

---
+++ actor/xfcesetter.go
@@ -0,0 +1,68 @@
+package actor
+
+import (
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"github.com/Sirupsen/logrus"
+)
+
+type XfceSetter int
+
+func (XfceSetter) Set(filename string) error {
+	path, err := filepath.Abs(filename)
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command(
+		"xfconf-query",
+		"-c",
+		"xfce4-desktop",
+		"-l",
+	)
+	bs, err := cmd.Output()
+	if err != nil {
+		logrus.Errorf("cannot read xfce4 desktop settings: %s", string(bs))
+	}
+
+	for _, line := range strings.Split(string(bs), "\n") {
+		logrus.Debugf("xfce-config output %v", line)
+		if strings.HasSuffix(line, "last-image") {
+			if err = setWithCommand(
+				"xfconf-query",
+				"-c",
+				"xfce4-desktop",
+				"-p",
+				line,
+				"-s",
+				path,
+			); err != nil {
+				logrus.Errorf(
+					"error during setting %s to %s: %s", line, path, err,
+				)
+				return err
+			}
+		} else if strings.HasSuffix(line, "image-style") {
+			if err = setWithCommand(
+				"xfconf-query",
+				"-c",
+				"xfce4-desktop",
+				"-p",
+				line,
+				"-s",
+				"5",
+			); err != nil {
+				logrus.Errorf("error during setting %s to 5: %s", line, err)
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	Setters.Register("xfce", XfceSetter(0))
+}
