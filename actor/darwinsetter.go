123e21fbfef66fcbb3671a323860135d6eddc78d
genzj <zj0512@gmail.com>
2020-07-16 10:27:23 -0700

    add a WA to force macos reload pic

--- actor/darwinsetter.go
+++ actor/darwinsetter.go
@@ -2,17 +2,25 @@
 
 package actor
 
-import "strconv"
+import (
+	"strconv"
+)
 
 type DarwinSetter int
 
 // Set set the desktop wallpaper to specified file by osascript
 func (_ DarwinSetter) Set(file string) error {
-	return setWithCommand(
+	err := setWithCommand(
 		"osascript",
 		"-e",
 		`tell application "System Events" to tell every desktop to set picture to `+strconv.Quote(file),
 	)
+	if err != nil {
+		return err
+	}
+
+	// WA: force Dock to reload the overwritten picture
+	return setWithCommand("killall", "Dock")
 }
 
 func init() {
--- actor/setter.go
+++ actor/setter.go
@@ -19,6 +19,7 @@ func setWithCommand(path string, arg ...string) error {
 	cmd := exec.Command(path, arg...)
 	logrus.Debugf("executing setter with command %s %v", path, arg)
 	if err := cmd.Start(); err != nil {
+		logrus.Errorf("executing setter failed: %s", err)
 		return err
 	}
 	go func() {
