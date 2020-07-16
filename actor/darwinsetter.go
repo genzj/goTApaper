809ddebea44135da43555c82809c40193cdd56c1
genzj <zj0512@gmail.com>
2020-07-16 10:27:37 -0700

    use unique wallpaper file name for macos in each setting

--- actor/darwinsetter.go
+++ actor/darwinsetter.go
@@ -3,24 +3,57 @@
 package actor
 
 import (
+	"fmt"
+	"path/filepath"
 	"strconv"
+	"strings"
+	"time"
+
+	"github.com/genzj/goTApaper/util"
+	"github.com/sirupsen/logrus"
 )
 
 type DarwinSetter int
 
 // Set set the desktop wallpaper to specified file by osascript
 func (_ DarwinSetter) Set(file string) error {
-	err := setWithCommand(
+	// MacOS Dock doesn't reload the wallpaper picture if its path is identical
+	// to current wallpaper's path, although the content of picture file has
+	// been overwritten
+	// A popular workaround is restart Dock by shell command "killall Dock". Howeverit has appreciable side-effect. For example if user is using the Misson Control during Dock restarting, Mission Control will be aborted.
+	// Here I use another solution that is using a unqiue wallpaper name each
+	// time by copying wallpaper picture to a file suffixed with timestamp. Old
+	// wallpaper files will be removed after a successful wallpaper setting.
+	dir, filename := filepath.Split(file)
+
+	if !strings.HasSuffix(dir, "/") {
+		dir = dir + "/"
+	}
+
+	wallpaperPath := fmt.Sprintf("%swallpaper_osx_%d%s", dir, time.Now().Unix(), filepath.Ext(filename))
+	_, err := util.CopyFile(file, wallpaperPath)
+	if err != nil {
+		return err
+	}
+
+	err = setWithCommand(
 		"osascript",
 		"-e",
-		`tell application "System Events" to tell every desktop to set picture to `+strconv.Quote(file),
+		`tell application "System Events" to tell every desktop to set picture to `+strconv.Quote(wallpaperPath),
 	)
 	if err != nil {
 		return err
 	}
 
-	// WA: force Dock to reload the overwritten picture
-	return setWithCommand("killall", "Dock")
+	if cleanErr := util.RemoveFilesByGlob(
+		dir+"wallpaper_osx_*.*",
+		func(path string) bool {
+			return path != wallpaperPath
+		},
+	); cleanErr != nil {
+		logrus.Warnf("error during cleaning up old wallpapers: %s", err)
+	}
+	return err
 }
 
 func init() {
--- util/path.go
+++ util/path.go
@@ -1,8 +1,13 @@
 package util
 
 import (
-	"github.com/sirupsen/logrus"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
 	"github.com/kardianos/osext"
+	"github.com/sirupsen/logrus"
 )
 
 // ExecutableFolder returns path to the folder containing currently running
@@ -13,4 +18,65 @@ func ExecutableFolder() string {
 		logrus.Fatal(err)
 	}
 	return folderPath
+}
+
+// CopyFile copy content of src to dst
+// credit to https://opensource.com/article/18/6/copying-files-go
+func CopyFile(src, dst string) (int64, error) {
+	sourceFileStat, err := os.Stat(src)
+	if err != nil {
+		logrus.Errorf("cannot get stat of the source %s: %s", src, err)
+		return 0, err
+	}
+
+	if !sourceFileStat.Mode().IsRegular() {
+		err = fmt.Errorf("%s is not a regular file", src)
+		logrus.Error(err)
+		return 0, err
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		logrus.Errorf("cannot open the source %s: %s", src, err)
+		return 0, err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		logrus.Errorf("cannot create destination file %s: %s", dst, err)
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	logrus.Debugf("%d bytes copied from %s to %s", nBytes, src, dst)
+	if err != nil {
+		logrus.Errorf("error in file copying %s", err)
+	}
+	return nBytes, err
+}
+
+type ConfirmRemove func(path string) bool
+
+// RemoveFilesByGlob deletes all files matching a specified glob pattern
+func RemoveFilesByGlob(pattern string, confirm ConfirmRemove) error {
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		logrus.Errorf("glob pattern %s failed: %s", pattern, err)
+		return err
+	}
+
+	for _, path := range matches {
+		if confirm(path) {
+			logrus.Debugf("remove %s confirmed", path)
+			if err = os.Remove(path); err != nil {
+				logrus.Errorf("remove %s failed: %s", path, err)
+				return err
+			}
+		} else {
+			logrus.Debugf("file %s skipped", path)
+		}
+	}
+
+	return nil
 }
